56c3384f568c584a0fe6b3d1be7843f526b8001f
Dds2 <85358022+dds256@users.noreply.github.com>
2024-08-11 15:44:53 +0530

    Update __init__.py

--- DAXXMUSIC/__init__.py
+++ DAXXMUSIC/__init__.py
@@ -4,7 +4,6 @@ from DAXXMUSIC.core.git import git
 from DAXXMUSIC.core.userbot import Userbot
 from DAXXMUSIC.misc import dbb, heroku
 
-from SafoneAPI import SafoneAPI
 from .logging import LOGGER
 
 dirr()
@@ -12,8 +11,7 @@ git()
 dbb()
 heroku()
 
-app = DAXX()
-api = SafoneAPI()
+app = DC()
 userbot = Userbot()
 
 
@@ -26,3 +24,4 @@ Spotify = SpotifyAPI()
 Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
+YTB = YTM()
